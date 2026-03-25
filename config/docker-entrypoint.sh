84e2b3c00b98755e48199b89d1b003117e749811
rbellaerts <immo@bellaerts.de>
2026-03-25 17:10:20 +0100

    fix: Write Kong config to /tmp to avoid permission denied

--- config/docker-entrypoint.sh
+++ config/docker-entrypoint.sh
@@ -2,10 +2,12 @@
 set -e
 
 # Substitute environment variables in kong.yml template using sed
-# (works on any base image without envsubst)
+# Write to /tmp/ because /var/lib/kong/ is not writable by kong user
 sed \
   -e "s|\${NEXT_PUBLIC_SUPABASE_ANON_KEY}|${NEXT_PUBLIC_SUPABASE_ANON_KEY}|g" \
   -e "s|\${SUPABASE_SERVICE_ROLE_KEY}|${SUPABASE_SERVICE_ROLE_KEY}|g" \
-  /var/lib/kong/kong.yml.template > /var/lib/kong/kong.yml
+  /var/lib/kong/kong.yml.template > /tmp/kong.yml
+
+export KONG_DECLARATIVE_CONFIG=/tmp/kong.yml
 
 exec /docker-entrypoint.sh kong docker-start
